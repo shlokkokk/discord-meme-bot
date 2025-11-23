cdba4d6919d8d4bcc670029bd07373e4966472ae
shlok <shlokshah412@gmail.com>
2025-11-23 10:41:37 +0530

    meme bot commited

---
+++ bot.py
@@ -0,0 +1,26 @@
+import discord
+import requests
+import json
+
+def get_meme():
+  response = requests.get('https://meme-api.com/gimme')
+  json_data = json.loads(response.text)
+  return json_data.get('url', "API error: Could not load meme 😭")
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('sendMeme'):
+            await message.channel.send(get_meme())
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('YOUR_BOT_TOKEN') 
